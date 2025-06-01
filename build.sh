014b6abdbd25afe1fb9d5db5890ec5bd5e7d817b
ashraf-revo <ashraf1abdelrasool@gmail.com>
2025-06-01 14:51:22 +0300

    add certmagic

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 xcaddy build \
-    --with github.com/cvhome-saas/caddy-domainlookup\
-    --with github.com/cvhome-saas/certmagic-s3
+    --with github.com/cvhome-saas/certmagic-s3\
+    --with github.com/cvhome-saas/caddy-domainlookup
 docker build -t saas-gateway -f Dockerfile .
