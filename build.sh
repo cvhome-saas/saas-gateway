0b3944c810b7ae7249167ca805b484aa319ea9f9
Ashraf Mahmoud <ashraf1abdlrasool@gmail.com>
2025-11-15 13:46:42 +0300

    init

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+xcaddy version
 xcaddy build \
     --with github.com/cvhome-saas/certmagic-s3\
     --with github.com/cvhome-saas/caddy-domainlookup
+./caddy version    
 docker build -t saas-gateway -f Dockerfile .
